f2b53f3e33aa784291dee810f609f3c7d4cd5900
Emanuel <emanuel.snasc1@gmail.com>
2025-02-06 12:33:48 -0300

    posts db reference

--- src/main/java/com/esn/workshopmongo/config/Instantiation.java
+++ src/main/java/com/esn/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.esn.workshopmongo.domain.Post;
 import com.esn.workshopmongo.domain.User;
+import com.esn.workshopmongo.dto.AuthorDTO;
 import com.esn.workshopmongo.repository.PostRepository;
 import com.esn.workshopmongo.repository.UserRepository;
 
@@ -30,13 +31,18 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, Instant.parse("2018-03-21T19:53:07Z"), "Partiu viagem",
-				"Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, Instant.parse("2018-03-23T19:53:07Z"), "Bom dia",
-				"Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, Instant.parse("2018-03-21T19:53:07Z"), "Partiu viagem",
+				"Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, Instant.parse("2018-03-23T19:53:07Z"), "Bom dia",
+				"Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		
+		userRepository.save(maria);
 	}
 
 }
