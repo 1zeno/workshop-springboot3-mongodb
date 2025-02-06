caf48500e7615a4a890278c760a281bba4c9053a
Emanuel <emanuel.snasc1@gmail.com>
2025-02-06 12:36:58 -0300

    get user posts

--- src/main/java/com/esn/workshopmongo/resources/UserResource.java
+++ src/main/java/com/esn/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.esn.workshopmongo.domain.Post;
 import com.esn.workshopmongo.domain.User;
 import com.esn.workshopmongo.dto.UserDTO;
 import com.esn.workshopmongo.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
